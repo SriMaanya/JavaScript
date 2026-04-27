f0ca8d8c47517ee880dfb9e209b3410eb28bcf8d
SriMaanya <rapolusrimaanya10@gmail.com>
2026-04-27 22:03:16 +0530

    Add files via upload

---
+++ Digital Clock/script.js
@@ -0,0 +1,20 @@
+
+function updateClock(){
+
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const merd = hours >= 12 ? 'PM' : 'AM';
+    hours = Number(hours) % 12 || 12;
+    hours = hours.toString().padStart(2, 0)
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("timer").textContent = timeString;
+
+    console.log(now)
+
+}
+
+
+updateClock()
+setInterval(updateClock,1000);
